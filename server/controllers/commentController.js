304164bfdceff64b750efb2d61a575476f233b46
himansu8 <himansusekharsahu50@gmail.com>
2024-03-26 01:34:31 +0530

    comments, like and dislike functionality added

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,23 +1,40 @@
-export async function addComment(req,res,next){
+import { createError } from "../error.js"
+import commentModel from "../models/commentModel.js"
+import videoModel from "../models/videoModel.js"
+
+
+
+
+export async function addComment(req, res, next) {
+    const newComment = new commentModel({ ...req.body, userId: req.user.id })
     try {
-        
+        const savedComment = await newComment.save()
+        res.status(200).send(savedComment)
     } catch (error) {
         next(error)
     }
 }
 
-export async function deleteComment(req,res,next){
+export async function deleteComment(req, res, next) {
     try {
-        
+        const comment = await commentModel.findById(res.params.id);
+        const video = await videoModel.findById(res.params.id);
+        if (req.user.id === comment.userId || req.user.id === video.userId) {
+            await commentModel.findByIdAndDelete(req.params.id);
+            res.status(200).json("The comment has been deleted.");
+        } else {
+            return next(createError(403, "You can delete ony your comment!"));
+        }
     } catch (error) {
         next(error)
     }
 }
 
 
-export async function getComments(req,res,next){
+export async function getComments(req, res, next) {
     try {
-        
+        const comments = await commentModel.find({ videoId: req.params.videoId });
+        res.status(200).json(comments);
     } catch (error) {
         next(error)
     }
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { createError } from "../error.js";
 import userModel from "../models/userModel.js";
+import videoModel from "../models/videoModel.js";
 
 
 export async function updateUser(req, res, next) {
@@ -46,11 +47,11 @@ export async function subscribe(req, res, next) {
     try {
         await userModel.findByIdAndUpdate(req.user.id, {
             $push: { subscribedUsers: req.params.id },
-          });
-          await userModel.findByIdAndUpdate(req.params.id, {
+        });
+        await userModel.findByIdAndUpdate(req.params.id, {
             $inc: { subscribers: 1 },
-          });
-          res.status(200).json("Subscription successfull.")
+        });
+        res.status(200).json("Subscription successfull.")
     } catch (error) {
         next(error)
     }
@@ -61,11 +62,11 @@ export async function unSubscribe(req, res, next) {
     try {
         await User.findByIdAndUpdate(req.user.id, {
             $pull: { subscribedUsers: req.params.id },
-          });
-          await User.findByIdAndUpdate(req.params.id, {
+        });
+        await User.findByIdAndUpdate(req.params.id, {
             $inc: { subscribers: -1 },
-          });
-          res.status(200).json("Unsubscription successfull.")
+        });
+        res.status(200).json("Unsubscription successfull.")
     } catch (error) {
         next(error)
     }
@@ -73,16 +74,28 @@ export async function unSubscribe(req, res, next) {
 
 
 export async function like(req, res, next) {
+    const id = req.user.id;
+    const videoId = req.params.videoId;
     try {
-
+        await videoModel.findByIdAndUpdate(videoId, {
+            $addToSet: { likes: id },
+            $pull: { dislikes: id }
+        })
+        res.status(200).json("The video has been liked.")
     } catch (error) {
         next(error)
     }
 }
 
 export async function disLike(req, res, next) {
+    const id = req.user.id;
+    const videoId = req.params.videoId;
     try {
-
+        await videoModel.findByIdAndUpdate(videoId, {
+            $addToSet: { dislikes: id },
+            $pull: { likes: id }
+        })
+        res.status(200).json("The video has been disliked.")
     } catch (error) {
         next(error)
     }
